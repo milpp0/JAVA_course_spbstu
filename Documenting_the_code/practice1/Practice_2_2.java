9c765ea6c0f06605ae18d777c63cacacc8e47778
milppo0 <tempusvitaeinmundo@gmail.com>
2025-12-06 13:19:12 +0300

    2.2

---
+++ Documenting_the_code/practice1/Practice_2_2.java
@@ -0,0 +1,23 @@
+package practice1;
+
+
+/**
+ * Краткое описание класса/метода.
+ * 
+ * @author vladi
+ * @version 1.0
+ */
+
+
+public class Practice_2_2 {
+	/**
+     * Выводит "Hello".
+     * @param String[] args
+     * @return Hello
+     */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
